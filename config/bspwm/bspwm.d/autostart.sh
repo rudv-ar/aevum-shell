84b286bfbd229a0fd7cb4edfdb7e0a0720c73da0
rudv-ar <rudv.ar.base@gmail.com>
2026-05-15 14:34:00 +0530

    modified qs startup timings and picom sync

--- config/bspwm/bspwm.d/autostart.sh
+++ config/bspwm/bspwm.d/autostart.sh
@@ -87,7 +87,7 @@ fi
 [[ $is_widget == true ]] && bash "$BSPDIR/widgets/$widget_bar/launch.sh" &
 [[ $is_plank == true ]] && plank &
 
-sleep 2
+sleep 5
 # ── Picom ─────────────────────────────────────────────────────────────────────
 if [[ $is_svc_picom == true ]]; then
     "$BSPDIR/bspwm.d/picomrc" &
