5a7955a342ea3ddb4b3764eb1d5adc82c811a20c
rudv-ar <rudv.ar.base@gmail.com>
2026-05-16 15:47:44 +0530

    integrated desktop borders with panes

--- config/bspwm/bspwm.d/exter_rules/raise_plank.sh
+++ config/bspwm/bspwm.d/exter_rules/raise_plank.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 bspc subscribe node_add node_remove node_focus desktop_focus | while read -r _; do
-  xdo raise -N "qs-border"
+  #xdo raise -N "qs-border"
+  xdo raise -N "qs-panes"
   xdo raise -N Plank
   xdo raise -N "qs-topbar"
   xdo raise -N "qs-notify"
--- config/bspwm/widgets/quickshell/launch.sh
+++ config/bspwm/widgets/quickshell/launch.sh
@@ -2,8 +2,8 @@
 
 qs -p ~/.config/bspwm/shell/topbar/shell.qml > /dev/null 2>&1 &
 topbar_pid=$!
-qs -p ~/.config/bspwm/shell/border/shell.qml > /dev/null 2>&1 &
-border_pid=$!
+qs -p ~/.config/bspwm/shell/panes/shell.qml > /dev/null 2>&1 &
+panes_pid=$!
 qs -p ~/.config/bspwm/shell/notifications/shell.qml > /dev/null 2>&1 &
 notifications_pid=$!
 qs -p ~/.config/bspwm/shell/powermenu/shell.qml > /dev/null 2>&1 & 
@@ -13,8 +13,8 @@ sleep 30
 topbar_wid=$(xdotool search --pid $topbar_pid)
 xdotool set_window --classname "qs-topbar" --class "qs-topbar" $topbar_wid
 #xprop -id $topbar_wid -f WM_CLASS 8s -set WM_CLASS "qs-topbar\000qs-topbar"
-border_wid=$(xdotool search --pid $border_pid)
-xdotool set_window --classname "qs-border" --class "qs-border" $border_wid
+panes_wid=$(xdotool search --pid $panes_pid)
+xdotool set_window --classname "qs-panes" --class "qs-panes" $panes_wid
 #xprop -id $border_wid -f WM_CLASS 8s -set WM_CLASS "qs-border\000qs-border"
 notifications_wid=$(xdotool search --pid $notifications_pid)
 xdotool set_window --classname "qs-notify" --class "qs-notify" $notifications_wid
