1394a92918a2acb1a2c6a71df15ecba886689774
rudv-ar <rudv.ar.base@gmail.com>
2026-05-20 19:48:56 +0530

    updated colorscheme

--- config/bspwm/bspwm.d/sources/wallpaper.sh
+++ config/bspwm/bspwm.d/sources/wallpaper.sh
@@ -18,4 +18,4 @@ wall_disfree_dir="$HOME/Workspace/Wallpapers"
 wall_single=true
 
 # the path of that single wallpaper
-wall_wallpaper="$wall_dir/mysticblue2.png"
+wall_wallpaper="$wall_dir/ign_yourName.png"
