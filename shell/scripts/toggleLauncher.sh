6e6859575e4c53d6481b07e6a45a03152b2dc356
rudv-ar <rudv.ar.base@gmail.com>
2026-05-31 20:48:57 +0530

    purged old shell

--- shell/scripts/toggleLauncher.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash 
-
-PREV_WIN=$(xdotool getactivewindow)
-
-
-xdotool windowfocus $(xdotool search --class "qs-launcher")
-qs -p ~/.config/bspwm/shell/launcher/shell.qml ipc call dock toggle
-
-
