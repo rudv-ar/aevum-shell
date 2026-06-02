19a3ae47447916896b0a757839fe038219868a98
rudv-ar <rudv.ar.base@gmail.com>
2026-06-02 19:30:15 +0530

    wallpaper changed

--- config/bspwm/bspwm.d/sources/wallpaper.sh
+++ config/bspwm/bspwm.d/sources/wallpaper.sh
@@ -18,4 +18,4 @@ wall_disfree_dir="$HOME/Pictures/Wallpapers"
 wall_single=true
 
 # the path of that single wallpaper
-wall_wallpaper="$wall_dir/ign-0011.png"
+wall_wallpaper="$wall_dir/ign_batman.png"
