44215ede30478bc5c349284ac7c95fa5943a3006
rudv-ar <rudv.ar.base@gmail.com>
2026-06-03 13:35:47 +0530

    fixed widget startup bug

--- config/bspwm/widgets/quickshell/launch.sh
+++ config/bspwm/widgets/quickshell/launch.sh
@@ -3,10 +3,10 @@
 
 qs -p ~/.config/aevum/shell/shell.qml &
 shell_pid=$!
-qs -p ~/.config/aevum/shell/notifications/shell.qml
+qs -p ~/.config/aevum/shell/notifications/shell.qml &
 notify_pid=$!
 
-sleep 60
+sleep 30
 shell_wid=$(xdotool search --pid $shell_pid)
 xdotool set_window --classname "qs-shell" --class "qs-shell" $shell_wid
 
