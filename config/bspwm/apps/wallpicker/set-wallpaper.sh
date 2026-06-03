32cd7c640c24fa9b334be5d4d9fc01f1968f4484
rudv-ar <rudv.ar.base@gmail.com>
2026-06-03 14:35:03 +0530

    fixed a small shell script bug

--- config/bspwm/apps/wallpicker/set-wallpaper.sh
+++ config/bspwm/apps/wallpicker/set-wallpaper.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-bsp_wall_dir="SHOME/.config/bspwm/bspwm.d/sources"
+bsp_wall_dir="$HOME/.config/bspwm/bspwm.d/sources"
 bsp_wall_source="$HOME/.config/bspwm/bspwm.d/sources/wallpaper.sh"
 cur_wall=$(cat "$bsp_wall_source" | perl -ne 'if ( /\$wall_dir\/([^"]*)/) { print "$1" }')
-mode="$(jq -r .mode ./config.json)"
+script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+mode_wall="$(jq -r .mode "$script_dir/config.json")"
+echo "$mode_wall"
 wallname=$(basename "$1")
-matugen image "$1" --type scheme-neutral --mode "$mode" --source-color-index 0 --json hex > ~/.config/aevum/shared/colors.json
+matugen image "$1" --type scheme-neutral --mode "$mode_wall" --source-color-index 0 --json hex > ~/.config/aevum/shared/colors.json
 feh --bg-fill "$1" && sed -i "s/$cur_wall/$wallname/g" "$bsp_wall_source"
 
--- config/bspwm/bspwm.d/sources/wallpaper.sh
+++ config/bspwm/bspwm.d/sources/wallpaper.sh
@@ -18,4 +18,4 @@ wall_disfree_dir="$HOME/Pictures/Wallpapers"
 wall_single=true
 
 # the path of that single wallpaper
-wall_wallpaper="$wall_dir/bhal-shogan.png"
+wall_wallpaper="$wall_dir/ign_batman.png"
