4f87611093439cd60afe2c53a0af9b46b011392a
rudv-ar <rudv.ar.base@gmail.com>
2026-06-05 23:07:20 +0530

    added my task manager

--- cli/installer/deps.sh
+++ cli/installer/deps.sh
@@ -119,8 +119,8 @@ AUR_DEPS=(
     neofetch
     cmatrix-git
     caffeine-ng
-    ttf-material-design-icons-git 
     xtitle
+    taskwarrior-tui
 )
 
 # ── Helpers ───────────────────────────────────────────────────────────────────
