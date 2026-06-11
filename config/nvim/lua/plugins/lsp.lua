8c14723df687fd86ba83fd6286dff43b52911620
rudv-ar <rudv.ar.base@gmail.com>
2026-06-11 22:47:57 +0530

    lsp disabled neatly

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -3,11 +3,15 @@ return {
         -- enable only specific lspservers
         -- I dont need all.
         "neovim/nvim-lspconfig",
-        enabled = false,
+        enabled = true,
         opts = {
             servers = {
                 clangd = {
                     enable = false, 
+                    on_attach = function(client)
+                        client.server_capabilities.documentFormattingProvider = false
+                        client.server_capabilities.documentRangeFormattingProvider = false
+                    end,                    
                 },  -- only for by beloved C
                 lua_ls = { enabled = false }, -- disable lua lsp server
             },
